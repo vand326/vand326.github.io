576ad403058641eb9ae2e08c6c9ff0c0ef444ed6
vand326 <42849525+vand326@users.noreply.github.com>
2021-06-16 18:38:10 -0700

    finished

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const modal = document.querySelector('.modal');
-const previews = document.querySelectorAll('.gallery');
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".image-gallery");
 const original = document.querySelector(".full-img");
-const imgText = document.querySelector(".caption");
+const caption = document.querySelector(".caption");
 
 previews.forEach(preview => {
     preview.addEventListener('click', ()=> {
       modal.classList.add("open");
-    })
+      original.classList.add("open");
+      //Dynamic change text and image
+      const originalSrc = preview.getAttribute("alts");
+      original.src=`images/${originalSrc}`
+      const altText = preview.alt;
+      caption.textContent = altText;
+    });
+});
 
-}
+modal.addEventListener('click', (e) => {
+  if(e.target.classList.contains('modal')){
+    modal.classList.remove("open");
+    original.classList.remove("open");
+  }
+});
